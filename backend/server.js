8c5d7624d9df9897a735d3b68eb57e5c37df3a86
Yatin Khatri <yatinkhatri@Yatins-MacBook-Air.local>
2026-04-26 00:37:24 +0530

    Fix 7

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // 2. MOUNT ROUTE
-app.use("/api/analyse", analyzeRoute);
+app.use("/analyse", analyzeRoute);
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
