0a78259e5b3c16d886ae823f56d385672e32f693
Yatin Khatri <yatinkhatri@Yatins-MacBook-Air.local>
2026-04-27 16:30:41 +0530

    fix: update default port to 8080

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
